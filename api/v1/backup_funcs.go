30d2b471168b290d2a916c3523651e6f78e85b64
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-12-16 13:39:03 +0100

    fix(backup): use instance session ID to detect instance manager restarts
    This commit improves the detection of instance manager restarts during
    backup operations by introducing an instance session ID instead of relying
    on the executable hash alone.
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- api/v1/backup_funcs.go
+++ api/v1/backup_funcs.go
@@ -63,12 +63,12 @@ func (backupStatus *BackupStatus) SetAsCompleted() {
 }
 
 // SetAsStarted marks a certain backup as started
-func (backupStatus *BackupStatus) SetAsStarted(podName, containerID, executableHash string, method BackupMethod) {
+func (backupStatus *BackupStatus) SetAsStarted(podName, containerID, sessionID string, method BackupMethod) {
 	backupStatus.Phase = BackupPhaseStarted
 	backupStatus.InstanceID = &InstanceID{
-		PodName:        podName,
-		ContainerID:    containerID,
-		ExecutableHash: executableHash,
+		PodName:     podName,
+		ContainerID: containerID,
+		SessionID:   sessionID,
 	}
 	backupStatus.Method = method
 }
